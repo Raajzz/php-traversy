9029431061ff4efcb1c08df816d1a19108a7ddf5
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-10-01 15:10:33 +0530

    Completed Arrays

--- tutorial/03_arrays.php
+++ tutorial/03_arrays.php
@@ -1,6 +1,38 @@
 <?php
-/* ----------- Arrrays ----------- */
 
-/*
-  Arrays hold multiple values. Each value in an array is called an "element"
-*/
+  $numbers = [1, 2, 3, 4];
+  $fruits = array("orange", "apple", "pear");
+
+
+  // print_r($numbers);
+  // print_r($fruits);
+  // var_dump($numbers);
+  // var_export($numbers); // will just echo how it'll look like in php code
+  // echo $fruits[2];
+
+  $people = [
+    "Raaj" => [
+      "Course" => "Status Making"
+    ],
+    "Nishant" => [
+      "Course" => "Valorant"
+    ],
+    "Arka" => [
+      "Course" => "Anime"
+    ],
+    "Kathi" => [
+      "Course" => [
+        "F1",
+        "Series",
+        "Music"
+      ]
+    ]
+      ];
+
+
+  // echo $people["Kathi"]["Course"][2];
+  // print_r($people);
+
+  echo json_encode($people);
+
+?>
