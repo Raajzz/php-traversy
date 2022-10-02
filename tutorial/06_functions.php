2c5754f7451ea5b2029f777af6d68037e7173b38
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-10-02 06:45:00 +0530

    Completed functions and some functions of arrays

--- tutorial/06_functions.php
+++ tutorial/06_functions.php
@@ -5,10 +5,57 @@
   Functions are reusable blocks of code that we can call to perform a specific task.
   We can pass values into functions to change their behavior. Functions have their own local scope as opposed to global scope
 
-
-/*
-** Function Syntax
-  function functionName($arg1, $arg2, ...) {
-    // code to be executed
+  
+  */
+  
+  function register_user(){
+    echo "User is registered successfully.";
   }
-*/
+
+  // register_user();
+
+  /* 
+  
+  There's function scope and global scope. 
+  The global scope won't work inside a function scope, we have define the variable as say "global" before anything works out.
+  
+  */
+
+  // stuff inside the definition is called an argument and stuff which you 'p'ass is called a 'p'arameter.
+
+  $y = 0;
+
+  function sum($n1, $n2){
+    global $y;
+    $y = $n1 + $n2;
+  }
+
+  // function print_y(){
+  //   // echo $y . "<br>"; error - because $y is undefined.
+  // }
+
+  // sum(10, 20);
+  // print_y();
+
+  // default arguments
+
+  function sum_default($n1 = 10, $n2 = 20){
+    return $n1 + $n2;
+  }
+
+  // echo sum_default();
+
+  // anon function
+
+  $subtract = function($n1, $n2){
+    return $n1 - $n2 . "<br>";
+  };
+
+  echo $subtract(10, 5);
+
+  // arrow function
+
+  $multiply = fn($n1, $n2) => $n1 * $n2;
+
+  echo $multiply(10, 20);
+?>
--- tutorial/07_array_functions.php
+++ tutorial/07_array_functions.php
@@ -1,7 +1,40 @@
 <?php
-/* --------- Array Functions -------- */
+/* --------- Working with Arrays and Array data -------- */
+
+  $fruits = ["apple", "orange", "pear"];
+
+  // to print the array
+  // print_r($fruits);
+
+  // find the length of the array
+  // echo count($fruits);
+
+  // to check if an element exists in an array
+  // var_dump(in_array("apple", $fruits));
+
+  // pushing elements infront of an array
+  $fruits[] = "Pineapple";
+  array_push($fruits, "Strawberry");
+
+  // pushing elements in the front
+  array_unshift($fruits, "mango"); // O(N) function, where 'N' is the size of the array
+
+  // remove from the end
+  array_pop($fruits);
+
+  // remove from the beginning
+  array_shift($fruits);
+
+  // remove a particular element
+  unset($fruits[2]); // removes the index too, therefore result will be 0 -> ... , 1 -> ... , 3 -> ... | no 2 index
+
+  print_r($fruits);
 
 /*
   Functions to work with arrays
   https://www.php.net/manual/en/ref.array.php
 */
+
+/**
+ * Read this - https://www.phpinternalsbook.com/php5/hashtables/basic_structure.html to get to know about arrays in detail
+ */
