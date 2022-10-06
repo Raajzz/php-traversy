d649c917bca57153d9f30cebd8bd649e4efeacec
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-10-06 23:13:25 +0530

    Completed file handling

--- tutorial/14_file_handling.php
+++ tutorial/14_file_handling.php
@@ -2,7 +2,23 @@
 
 /* ---------- File Handling --------- */
 
-/* 
-  File handling is the ability to read and write files on the server.
-  PHP has built in functions for reading and writing files.
-*/
+$file = 'extras/users.txt';
+
+if(file_exists($file)){
+
+  // echo readfile($file); 
+  // to just read a file
+  // it'll also print the size of the file at the end
+
+  $handle = fopen($file, 'r');
+  $contents = fread($handle, filesize($file));
+  fclose($handle);
+  echo $contents;
+} else {
+  $handle = fopen($file, 'w');
+  $contents = 'Brad'.PHP_EOL.'Sara'.PHP_EOL.'Mike';
+  fwrite($handle, $contents);
+  fclose($handle);
+}
+
+?>
