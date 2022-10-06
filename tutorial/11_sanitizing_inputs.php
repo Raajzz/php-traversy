d9ec69bdbc6ac23c0ea760055375af488b52f116
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-10-06 20:02:05 +0530

    Completed sanitizing inputs and cookies

--- tutorial/11_sanitizing_inputs.php
+++ tutorial/11_sanitizing_inputs.php
@@ -4,3 +4,33 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+
+?>
+
+<?php
+  // both of the below ways work to sanitize the inputs
+  if(isset($_POST['submit'])){
+
+    // method 1
+    // $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    // $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    // method 2
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $age = filter_var($_POST['age'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+    echo "${name} ${age} <br>";
+  }
+?>
+
+<form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="text" name="age" id="">
+  </div>
+  <input type="submit" value="Enter" name="submit">
+</form>
--- tutorial/12_cookies.php
+++ tutorial/12_cookies.php
@@ -4,3 +4,14 @@
 /*
   Cookies are a mechanism for storing data in the remote browser and thus tracking or identifying return users. You can set specific data to be stored in the browser, and then retrieve it when the user visits the site again.
 */
+// setting a cookie
+setcookie('name', 'brad', time() + 86400);
+echo time();
+if(isset($_COOKIE['name'])){
+  echo $_COOKIE['name'];
+} else {
+  echo "Cookies haven't been set yet.";
+}
+setcookie('name', 'This string can be anything', time() - 86400);
+// I guess the cookie expired because, cookies expires of the time of setting them goes below the time of creating a cookie.
+?>
